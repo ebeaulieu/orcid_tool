a2a1067c99b36a39fa309a3e2100c6e08747a5c2
ebeaulieu <e_beaulieu@hotmail.com>
2019-10-24 17:28:12 -0400

    added new files

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
 Cet outil permet de produire une liste des publications des membres d'un groupe de recherche.
 
-- Fournir en entrée le fichier list_members.csv avec nom de famille (last_name), prénom (first_name), statut (status), adresse courriel, (email) et numéro d'identifiant ORCID (ORCID). Le fichier list_members.csv est une copie de l'onglet ORCID_export du document de suivi des membres :
+- Fournir en entrée le fichier list_members.csv avec nom de famille (last_name), prénom (first_name), statut (status), adresse courriel (email) et numéro d'identifiant ORCID (ORCID). Le fichier list_members.csv est une copie de l'onglet ORCID_export du document de suivi des membres :
 https://www.dropbox.com/scl/fi/verl2khb2sd3e0lbl928o/Suivi_ORCID_GRIL.xlsx?cloud_editor=excel&dl=0&web_open_id=web_open_id-51e5bb79a8c360a7
 
-- Produit en sortie le fichier export.csv avec prenom, nom, doi, putcode, type, revue, issn, editeur, titre annee, romeo_colour et	url_for_pdf
+Niveau Chercheur
+	- Extrait une liste d'identifiants de travaux (putcode) de l'API d'ORCID en fournissant l'identifiant ORCID du chercheur
+	Référence : https://pub.orcid.org
+	https://members.orcid.org/api/tutorial/reading-xml
+
+Niveau Travail (API d'ORCID, CrossRef)
+	- Extrait les informations suivantes de l'API d'ORCID en fournissant l'identifiant ORCID du chercheur et l'identifiant du travail (putcode) : 
+		- l'identifiant DOI du travail (doi)
+		- le type de document (type)
+		- l'identidiant de revue (issn)
+		- le volume, le numéro et l'intervalle de pages sont extraits de la citation bibtex (volume, number, pages)
+		- le titre du travail (title)
+		- l'année de publication (publication_year)
+		- la liste des auteurs (authors)
+	Référence : https://pub.orcid.org
+
+	- Extrait l'URL du PDF si disponible (url_for_pdf) de l'API public d'ORCID en fournissant le doi obtenu avec l'API d'ORCID
+	Référence : http://unpaywall.org/data-format
+	
+	- Extrait le nombre de citations (times_cited) de l'API de Dimensions Metrics en fournissant le doi obtenu avec l'API d'ORCID
+	Exemple : https://metrics-api.dimensions.ai/doi/10.1071/EN11140
+
+Niveau Revue
+	- Extrait le nom de la revue de l'API CrossRef avec l'identifiant de revue (issn) obtenu avec l'API d'ORCID : 
+	- le nom de la revue (journal)
+	- le nom de l'éditeur (publisher)
+
+Niveau Éditeur
+
+- Extrait le nom de l'éditeur de l'API CrossRef avec l'identifiant de revue (issn) obtenu avec l'API d'ORCID
+- Extrait la couleur RoMEO (romeo_colour) de l'éditeur de l'API SHERPA/RoMEO avec l'identifiant de revue (issn) obtenu avec l'API d'ORCID
+Exemple : http://www.sherpa.ac.uk/romeo/api29.php?issn=0956-7135&ak=tEKXGkhWygY
+
+Pour en savoir plus sur les couleurs RoMEO
+http://www.sherpa.ac.uk/romeoinfo.html : 
+Pour en savoir plus sur l'utilisation de l'API : 
+http://sherpa.ac.uk/romeo/apimanual.php?la=en&fIDnum=|&mode=simple#howto
+
+- Produit en sortie un fichier csv avec first_name, last_name, doi, putcode, type, journal, issn, publisher, title, publication_year, romeo_colour, url_for_pdf, times_cited et authors
 
 Développement prévu :
 
@@ -11,3 +49,5 @@ Développement prévu :
 - Ne Permettre d'ajouter que les nouvelles publications par rapport à ce qui est déjà présent dans le fichier export.csv
 - Algorithme de name matching pour parvenir à identifier les coauteurs membres du GRIL
 - Documenter
+- Intégration directe dans une base de données Access
+- Séparer le code interrogeant avec le doi pour ne faire des requêtes qu'avec une liste de doi uniques
--- orcid.py
+++ orcid.py
@@ -1,9 +1,9 @@
 ####################################################################################################
 #
-# Avec une liste d'identifiants ORCID, ce petit programme extrait de l'API de
-# ORCID tous les travaux inscrits pour chaque chercheur et exporte un fichier
-# csv comprenant une liste de publications incluant prenom, nom, putcode,
-# type, revue, issn, titre, annee et auteurs
+# Avec une liste d'identifiants ORCID, ce petit programme extrait des APIs de
+# ORCID, unpaywall, Dimensions Metrics et CrossRef tous les travaux inscrits
+# pour chaque chercheur et exporte un fichier csv comprenant une liste de
+# publications incluant prenom, nom, putcode, type, revue, issn, titre, annee et auteurs
 #
 ####################################################################################################
 
@@ -11,9 +11,16 @@ import requests
 from xml.etree import ElementTree as ET
 import json
 import os, csv
+import bibtexparser
 
-os.chdir('C:\\Users\Eric\Documents\Suivi des publications\ORCID\Outil\orcid_tool')
-fichier_sortie = "export2.csv"
+# Pour Git cd ~/Documents/SuiviPublications/ORCID/Outil/orcid_tool
+os.chdir('C:\\Users\Eric\Documents\SuiviPublications\ORCID\Outil\orcid_tool')
+
+import time, datetime
+timestr = time.strftime("%Y%m%d-%H%M%S")
+#print timestr
+
+fichier_sortie = "PublicationsGRIL_"+timestr+".csv"
 
 with open('list_members.csv','r', encoding='utf-8') as infile:
     reader = csv.reader(infile)
@@ -24,8 +31,8 @@ with open('list_members.csv','r', encoding='utf-8') as infile:
 
 chercheurs = []
 for member in members:
-    #if member['status'] == 'Régulier' and member['orcid'] != '':
-    if member['last_name'] == 'Talbot':
+    if member['status'] == 'Régulier' and member['orcid'] != '':
+    #if member['last_name'] == 'Beisner':
         chercheurs.append(member['orcid'])
 
 #print(regular_members)
@@ -33,13 +40,16 @@ for member in members:
 f1 = open(fichier_sortie, 'w', newline='', encoding='utf-8')
 with f1:
     writer = csv.writer(f1)
-    writer.writerow(['prenom','nom','doi','putcode','type','revue','issn','editeur','titre','annee','romeo_colour','url_for_pdf'])
+    writer.writerow(['first_name','last_name','doi','putcode','source','type','journal','volume','number','pages','issn','publisher','title','publication_year','romeo_colour','url_for_pdf','times_cited','authors'])
 
 for chercheur in chercheurs:
     resp_orcid = requests.get("http://pub.orcid.org/"+chercheur, headers={'Accept':'application/orcid+json'})
     prenom = resp_orcid.json()["person"]["name"]["given-names"]["value"]
     nom = resp_orcid.json()["person"]["name"]["family-name"]["value"]
     #print('\n'+prenom+' '+nom+'\n')
+
+    print('\n'+prenom+' '+nom +' ('+chercheur+')')
+    start = time.time()
 
     putcodes = []
 
@@ -50,6 +60,10 @@ for chercheur in chercheurs:
         for work_summary in group["work-summary"]:
             putcodes.append(work_summary["put-code"])
             #print(work_summary["put-code"])
+            if work_summary["source"]["source-name"]["value"]:
+                source = work_summary["source"]["source-name"]["value"]
+            else:
+                source = 'unknown'
 
     for putcode in putcodes:
         auteurs=[]
@@ -57,51 +71,91 @@ for chercheur in chercheurs:
 
         doi=''
         url_for_pdf=''
+        times_cited=''
         issn=''
         revue=''
         editeur=''
         authors=[]
         romeo_colour=''
+
         try:
-            for external_id in resp_orcid.json()["external-ids"]["external-id"]:
-                if "external-id-type" in external_id:
+            if "external-ids" in resp_orcid.json():
+                for external_id in resp_orcid.json()["external-ids"]["external-id"]:
+                    if "external-id-type" in external_id:
                         if external_id["external-id-type"] == "doi":
                             doi = external_id["external-id-value"]
 
-                            #ajout unpaywall
-                            email = 'eric.g.beaulieu@umontreal.ca'
-                            resp_unpaywall = requests.get('https://api.unpaywall.org/v2/'+doi+'?email='+email)
-                            value = resp_unpaywall.json()["best_oa_location"]["url_for_pdf"]
-                            if value:
-                                url_for_pdf = value
-                                #print(url_for_pdf)
-                            else:
-                                url_for_pdf = ''
+                            # API unpaywall
+                            try:
+                                email = 'eric.g.beaulieu@umontreal.ca'
+                                url = 'https://api.unpaywall.org/v2/'+doi+'?email='+email
+                                resp_unpaywall = requests.get(url)
+                                if resp_unpaywall.ok is True:
+                                    if resp_unpaywall.json()["best_oa_location"]:
+                                        url_for_pdf = resp_unpaywall.json()["best_oa_location"]["url_for_pdf"]
+                                        #print(url_for_pdf)
+                            except:
+                                #pass
+                                print('Something went wrong with unpaywall API with request',url)
+                                #url_for_pdf = ''
 
-                            resp_crossref = requests.get("https://api.crossref.org/works/"+doi)
-                            issn = resp_crossref.json()["message"]["ISSN"][0]
-                            editeur = resp_crossref.json()["message"]["publisher"]
-                            for rang, author in enumerate(resp_crossref.json()["message"]["author"]):
-                                authors.append({"rang":rang+1,"given":author["given"]})
-                                authors[len(authors)-1]["family"] = author["family"]
-                                if "ORCID" in author:
-                                    authors[len(authors)-1]["orcid"] = author["ORCID"]
-                                #print(authors[len(authors)-1])
-                            resp_crossref = requests.get("https://api.crossref.org/journals/"+issn)
-                            revue = resp_crossref.json()["message"]["title"]
+                            # API Dimensions Metrics
+                            try:
+                                url = 'https://metrics-api.dimensions.ai/doi/'+doi
+                                resp_dimensions = requests.get(url)
+                                if resp_dimensions.ok is True:
+                                    value = resp_dimensions.json()["times_cited"]
+                                    if value:
+                                        times_cited = value
+                                        #print(times_cited)
+                            except:
+                                #pass
+                                print('Something went wrong with Dimensions Metrics API for request',url)
+                                #times_cited = ''
+
+                            # API CrossRef
+                            try:
+                                url = "https://api.crossref.org/works/"+doi
+                                resp_crossref = requests.get(url)
+                                if resp_crossref.ok is True:
+                                    if "ISSN" in resp_crossref.json()["message"]:
+                                        issn = resp_crossref.json()["message"]["ISSN"][0]
+                                    if "publisher" in resp_crossref.json()["message"]:
+                                        editeur = resp_crossref.json()["message"]["publisher"]
+                                    for rang, author in enumerate(resp_crossref.json()["message"]["author"]):
+                                        authors.append({"rang":rang+1,"given":author["given"]})
+                                        authors[len(authors)-1]["family"] = author["family"]
+                                        if "ORCID" in author:
+                                            authors[len(authors)-1]["orcid"] = author["ORCID"]
+                                        #print(authors[len(authors)-1])
+                                    resp_crossref = requests.get("https://api.crossref.org/journals/"+issn)
+                                    if "title" in resp_crossref.json()["message"]:
+                                        revue = resp_crossref.json()["message"]["title"]
+                            except:
+                                #pass
+                                print('Something went wrong with CrossRef API with request',url)
+                                #revue=''
 
                             #ajout SHERPA/RoMEO
-                            romeo_key = "tEKXGkhWygY"
-                            resp_romeo = requests.get("http://www.sherpa.ac.uk/romeo/api29.php?issn="+issn+"&ak="+romeo_key)
-                            value = ET.fromstring(resp_romeo.text.strip()).find('publishers/publisher/romeocolour')
-                            if value:
-                                romeo_colour = value.text
-                                print(romeo_colour)
-                            else:
-                                romeo_colour = ''
-
+                            try:
+                                romeo_key = "tEKXGkhWygY"
+                                if issn != '':
+                                    url = "http://www.sherpa.ac.uk/romeo/api29.php?issn="+issn+"&ak="+romeo_key
+                                    resp_romeo = requests.get(url)
+                                    if resp_romeo.ok:
+                                        resp_romeo_ET = ET.fromstring(resp_romeo.text.strip())
+                                        value = resp_romeo_ET.find('publishers/publisher/romeocolour')
+                                        if value is not None and value.text:
+                                            romeo_colour = value.text
+                                            #print(romeo_colour)
+                            except:
+                                #pass
+                                print('Something went wrong with SHERPA/RoMEO API with request',url)
+                                #romeo_colour = ''
         except:
-            doi=''
+            #pass
+            print('Something went wrong with DOI acquisition for putcode',putcode)
+            #doi=''
 
         #print(json.dumps(resp_orcid.json(), sort_keys=True, indent=4, separators=(',', ': ')))
         type = resp_orcid.json()["type"]
@@ -142,6 +196,16 @@ for chercheur in chercheurs:
             annee = ''
 
         try:
+            if resp_orcid.json()["citation"]["citation-type"] == "bibtex":
+                bib_database = bibtexparser.loads(resp_orcid.json()["citation"]["citation-value"])
+                volume = bib_database.entries[0]["volume"]
+                number = bib_database.entries[0]["number"]
+                pages = bib_database.entries[0]["pages"]
+                #print(volume+'('+number+'):'+pages)
+        except:
+            volume,number,pages = '','',''
+
+        try:
             for rang, contributor in enumerate(resp_orcid.json()["contributors"]["contributor"]):
                 #print(str(rang)+" : "+contributor["credit-name"]["value"])
                 auteurs.append(contributor["credit-name"]["value"])
@@ -150,10 +214,10 @@ for chercheur in chercheurs:
             auteurs=[]
 
         #print(prenom+' '+nom+'*'+doi+'*'+str(putcode)+'*'+type+'*'+revue+'*'+issn+'*'+titre+'*'+str(annee)+'*['+'*'.join(auteurs)+']')
-        print(nom+', '+prenom+' ('+str(annee)+') '+titre)
+        #print(nom+', '+prenom+' ('+str(annee)+') '+titre)
 
         #nms = [prenom,nom,doi,str(putcode),type,revue,issn,titre,str(annee),'*'.join(auteurs)]
-        nms = [prenom,nom,doi,str(putcode),type,revue,issn,editeur,titre,str(annee),romeo_colour,url_for_pdf]
+        nms = [prenom,nom,doi,str(putcode),source,type,revue,volume,number,pages,issn,editeur,titre,str(annee),romeo_colour,url_for_pdf,times_cited,'*'.join(auteurs)]
         #nms = [prenom,nom,doi]
 
         #os.chdir('C:\\Users\Eric\Documents\siteweb')
@@ -165,3 +229,13 @@ for chercheur in chercheurs:
 
             #for row in nms:
             writer.writerow(nms)
+
+        #print('.', end='')
+        #print(putcode, end=' ')
+
+    end = time.time()
+    a = datetime.timedelta(seconds=end-start)
+    # Référence : https://stackoverflow.com/a/1384465
+    print(str(a), end='')
+    #print('', end - start, 'seconds', end='')
+    # Référence : https://stackoverflow.com/a/7370824
